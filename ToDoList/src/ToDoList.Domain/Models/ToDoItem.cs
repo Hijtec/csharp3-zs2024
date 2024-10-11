9d59eca7ec02aaef32643ecc7474cd84090c8968
Martin Černil <martin.cernil@thermofisher.com>
2024-10-11 14:44:48 +0200

    l03

---
+++ ToDoList/src/ToDoList.Domain/Models/ToDoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoList.Domain.Models;
+
+public class ToDoItem
+{
+    public int ToDoItemId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public bool IsCompleted { get; set; }
+}
